f54d6488bbbd644f1efd91ed0b6f84f94fb685f9
Aditya-cup <25113cn327@glbitm.ac.in>
2026-02-11 12:11:46 +0530

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+
+    printf("Enter a number");
+    scanf("%d", &a);
+
+    if (a % 400 == 0) 
+        printf("%d is a leap year.\n", a);
+     else if (a % 100 == 0) 
+
+        printf("%d is not a leap year.\n", a);
+
+     else if (a% 4 == 0) 
+
+        printf("%d is a leap year.\n", a);
+     else 
+     printf("%d is not a leap year.\n", a);
+    
+    
+    return 0;
+
+}
